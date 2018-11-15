dfe8fc41a108b5124355d0c29cb704e6e0ebf5a1
frank <546587684@qq.com>
2018-11-15 10:26:35 +0800

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,14 @@
+// routes.js
+module.exports = {
+    "/api/v1/tableau/treemap": "/treeMap",
+    "/api/v1/tableau/growth-of-unit": "/growthOfUnit",
+    "/api/v1/tableau/point-to-point": "/pointToPoint",
+    "/api/v1/tableau/aggregate-plan-overview": "/apo",
+    "/api/v1/tableau/flows-by-classification": "/flowByClassification",
+    "/api/v1/tableau/manager-fee": "/managerFee",
+    "/api/v1/tableau/contribution-to-return": "/contributionToReturn",
+    "/api/v1/tableau/pbc": "/pbc",
+    "/api/v1/tableau/country-analysis": "/countryAnalysis",
+    "/api/v1/tableau/contribution-to-return-new": "/contributionToReturnNew",
+    "/api/dataformat": "/dataformat",
+}
